003411f5cf1acc8ac183c55228e78143a17275f4
João Gabriel <cirinojoao261@gmail.com>
2026-04-28 09:56:30 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,17 @@ import Model.Filme;
 public class Main {
     public static void main(String[] args) {
 
-        Cliente cliente = new Cliente("Vinicius", "(55) +35 9894-2122");
+        Cliente cliente = new Cliente("João", "(55) +35 99888-2684");
+        System.out.println(cliente.getNome());
+        System.out.println(cliente.getContato());
+
 
         Filme filme = new Filme ("Smurfs", 10.0,"Infantil");
-        filme.aplicardesconto(5);
+        System.out.println(filme.getValoraluguel());
+        System.out.println(filme.getGenero());
+        System.out.println(filme.getTitulo());
 
         Aluguel aluguel = new Aluguel(cliente, filme, 4);
-
-        System.out.println(filme.exibirdetalhes());
         System.out.println(aluguel.getResumo());
         System.out.println(cliente.apresentar());
     }
